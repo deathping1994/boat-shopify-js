9300ad22ca9299b8f30cf7fee9a9c044b627af83
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-03-15 19:05:14 +0530

    Update function-product.js

--- function-product.js
+++ function-product.js
@@ -392,7 +392,7 @@ $(document).ready(function () {
 
 
             // $(".single-option-selector").val(current_title).change();
-            setTimeout(function(){ hideLoading();}, 500);
+            setTimeout(function(){ hideLoading();}, 1000);
            
         } else if (shop_name == "boat-nirvana.myshopify.com") {
 
