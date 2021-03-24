8c7a29cd76e4b55ad234c477f65bb3ccc2e9c99a
Swapnil Rajawat <48756156+hacktivist1512@users.noreply.github.com>
2021-03-24 12:39:03 +0530

    Update function-product.js

--- function-product.js
+++ function-product.js
@@ -81,18 +81,20 @@ function hideLoading() {
 }
 
 $(document).on('productDataLoaded', function() {
-    
+
     function isElementInViewport(el) {
         if (typeof jQuery === "function" && el instanceof jQuery) {
             el = el[0];
         }
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && 
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
-        );
+        if(el){
+            var rect = el.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        }
     };
     var product_list_mobile = $(".prodct_variant").clone();
     product_list_mobile.find('li').each(function() {
