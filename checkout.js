a4971276623fd169b3e02673de4b33d22a34f104
Swapnil Rajawat <48756156+hacktivist1512@users.noreply.github.com>
2021-04-23 19:18:25 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -381,6 +381,7 @@ $(document).on(`page:load page:change`, function () {
   <div class="commander-wrapper">
       <input placeholder="Discount Code" class="commander-input" data-discount-field="true" autocomplete="off" aria-required="true" size="30" type="text" name="checkout[reduction_code]">
     </div>
+
     <button name="button" type="submit" class="commander-btn" aria-busy="false">
           <span class="btn__content visually-hidden-on-mobile" aria-hidden="true">
             Apply
@@ -391,14 +392,21 @@ $(document).on(`page:load page:change`, function () {
           <svg class="icon-svg icon-svg--size-16 btn__icon shown-on-mobile" aria-hidden="true" focusable="false"> <use xlink:href="#arrow"></use> </svg>
           <svg class="icon-svg icon-svg--size-18 btn__spinner icon-svg--spinner-button" aria-hidden="true" focusable="false"> <use xlink:href="#spinner-button"></use> </svg>
 </button>      </div>
+
 </div>`)
 $('.commander-input')[0].style.display = "none";
 $('.commander-btn')[0].style.display = "none";
 
+var token = '';
+
+$.get('/cart.js').then(response => {
+  token = $.parseJSON(response).token
+})
+
 
   $('.order-summary__section--discount .field__input-btn').on("click touchstart", function(){
         var basecode = $('#checkout_reduction_code')[0].value
-        $.ajax({type:"POST",url:'https://boat-api.farziengineer.co/discount', headers:{"Content-Type": "application/json"},data: `{"code":"${basecode}"}`}).then(response => {  
+        $.ajax({type:"POST",url: 'https://boat-api.farziengineer.co/discount', headers:{"Content-Type": "application/json"},data: `{"code":"${basecode}", "cartId":"${token}"}`}).then(response => {  
         if((response == "true") || (response == "True")){
             $('.commander-input')[0].value = basecode;
             $('.commander-btn').click();
