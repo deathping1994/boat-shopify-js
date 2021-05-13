36392dda06dd96bb4b5ae040aadd6e135c29aa09
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2021-05-13 19:16:06 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -404,7 +404,8 @@ $.get('/cart.js').then(response => {
 })
 
 
-  $('.order-summary__section--discount .field__input-btn').on("click touchstart", function(){
+  $('.order-summary__section--discount .field__input-btn').on("click touchstart", function(event){
+        event.preventDefault()
         var basecode = $('#checkout_reduction_code')[0].value
         $.ajax({type:"POST",url: 'https://boat-api.farziengineer.co/discount', headers:{"Content-Type": "application/json"},data: `{"code":"${basecode}", "cartId":"${token}"}`}).then(response => {  
         if((response == "true") || (response == "True")){
@@ -416,28 +417,6 @@ $.get('/cart.js').then(response => {
           $('.commander-btn').click();
         })
   })
-  
-  if($('.section__content .field__input-btn').length>0){
-        $("#error-for-reduction_code").attr( "style", "display: none !important;" )
-        setTimeout(function(){
-            $("#error-for-reduction_code").removeAttr('style')
-        }, 4000);
-        $('.section__content .field__input-btn').on("click touchstart", function(){
-        var basecode = $('#checkout_reduction_code_mobile')[0].value
-        $.ajax({type:"POST",url: 'https://boat-api.farziengineer.co/discount', headers:{"Content-Type": "application/json"},data: `{"code":"${basecode}", "cartId":"${token}"}`}).then(response => {  
-        if((response == "true") || (response == "True")){
-            $('.commander-input')[0].value = basecode;
-            $('.commander-btn').click();
-          } 
-        }).catch(() => {
-          $('.commander-input')[0].value = basecode;
-          $('.commander-btn').click();
-        })
-
-  })}
- 
-
-
 
   $('.field__input-btn.btn').on("click", function (event) {
     var txt = $('#checkout_reduction_code')[0].value;
